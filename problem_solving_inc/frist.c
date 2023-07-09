cc743a9ab2c75c5d9761ed0c2a62e6cb19fcd203
sankalp kumar <65161219+sankalp475@users.noreply.github.com>
2023-07-09 13:41:37 +0530

    new commit

--- problem_solving_inc/frist.c
+++ problem_solving_inc/frist.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int  main(void) {
+
+int main(void) {
 
 	printf(" first c program  ");
-	return 0;
+	for (int i=0; i <= 5; i++) {
+		printf("[user%d]:hello\n", i);
+	}
+		return 0;
 }
