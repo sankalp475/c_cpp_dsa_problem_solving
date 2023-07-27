1e0a939bbd4cbfd263f8b3328a57d8c40c122f79
sankalp <sankalp05ot@gmail.com>
2023-07-27 16:02:38 +0530

    wtf: completed  half assignment 2

---
+++ ProblemSolving_in_C/day 1 printf and scanf/input_output02.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int  main(void) {
+
+	// int x, y, z;
+	// scanf("%d %d %d", &x, &y, &z);
+	// float x1 = x + y + z;
+	// printf(" the average fo three number:(%d %d %d) is %f", x, y, z, x1/3);
+
+	// int r;
+	// scanf("%d", &r);
+	// float pi = 3.14159265359, c = 2 * pi * r;
+
+	// int si, p,r1,t;
+	// printf("enter p,r,t where P = Principal, R = Rate of Interest, and T = Time period \n ");
+	// scanf("%d %d %d", &p, &r1, &t);
+	// si = (p * r1 * t)/100;
+	// printf("simple intrest is %d",si);
+
+	// int l, w, h;
+	// printf("Enter the length, bredth, height: ");
+	// scanf("%d %d %d", &l, &w, &h);
+	// printf(" volume of cuboid is %d", l * w * h);
+
+	// int cp,sp;
+	// printf("Enter the cost prise per dezen: ");
+	// scanf("%d", &cp);
+	// printf("Enter the selling prise per dezen: ");
+	// scanf("%d", &sp);
+	// int profit = (sp-cp)*25/12;
+	// printf(" profit for selling 25 bannas is %d \n",profit);
+
+	char x;
+	printf(" Enter the character: ");
+	scanf("%c", &x);
+	printf(" ascii code for this character '%c' is %d ", x, x);
+	return 0;
+}
