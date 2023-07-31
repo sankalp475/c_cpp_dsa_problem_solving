1f31ace8ce4e1b2249e851118c723d4a37778303
sankalp <sankalp05ot@gmail.com>
2023-07-31 13:29:58 +0530

    make last digit of number stored as zero

--- ProblemSolving_in_C/Operaters/operater02.c
+++ ProblemSolving_in_C/Operaters/operater02.c
@@ -6,8 +6,31 @@ int  main(void) {
 	// int num;
 	// printf("Enter the number: ");
 	// scanf("%d", &num);
-	// printf("the sum of its digit are: %d", num % 10 + num / 10 % 10 + num / 10 / 10);
+	// printf("the sum of its digit are: %d\n", num % 10 + num / 10 % 10 + num / 10 / 10);
 
+	 //! asci code of character plus
+	// printf("the ascii code of charccter ' %c' is %d\n", '+', '+');
+
+	//! size of float char int double
+	// printf("\n the size of int is %d\n the size of char is %d\n the size of float is %d\n the size of double is %d\n", sizeof(int), sizeof(char), sizeof(float), sizeof(double));
+
+	//! make last digit of number stored as zero eg: 2345 -> 2340
+	// int num2;
+	// printf("Enter the number: ");
+	// scanf("%d", &num2);
+	// printf("\nnumber before: %d\n",num2);
+	// num2 = num2 / 10 * 10;
+	// printf("number after: %d\n",num2);
+
+	//! wap which take 2 input and one numer and one digit and make the last digit of number as 2nd input
+
+	// int n1, d1;
+	// printf("enter a number and a digit: ");
+	// scanf("%d %d", &n1, &d1);
+	// printf("\nnumber before: %d\n", n1);
+	// n1 = n1 / 10 * 10 + d1;
+	// printf("number after: %d", n1);
+	// printf(" one digit %d is added\n", d1);
 
 	return 0;
 }
