6b0bdbe38326ac9ffd72be5e48396c6c4282aa43
sankalp <sankalp05ot@gmail.com>
2023-07-31 13:01:58 +0530

    count number of digit in a number;

--- ProblemSolving_in_C/Operaters/operater02.c
+++ ProblemSolving_in_C/Operaters/operater02.c
@@ -3,9 +3,11 @@
 int  main(void) {
 
 	//!  program 1: count number of digit in a number;
-	int num;
-	printf("Enter the number: ");
-	scanf("%d", &num);
-	printf("the sum of its digit are: %d", num % 10 + num / 10 % 10 + num / 10 / 10);
+	// int num;
+	// printf("Enter the number: ");
+	// scanf("%d", &num);
+	// printf("the sum of its digit are: %d", num % 10 + num / 10 % 10 + num / 10 / 10);
+
+
 	return 0;
 }
